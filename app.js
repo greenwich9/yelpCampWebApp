b58a3ae983691288680997ecd8661e3103858195
EC2 Default User <ec2-user@ip-172-31-23-170.us-east-2.compute.internal>
2018-05-15 02:01:39 +0000

    fix bug

--- app.js
+++ app.js
@@ -47,9 +47,9 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(process.env.PORT, process.env.IP, function() {
-    console.log("started");
-});
+// app.listen(process.env.PORT, process.env.IP, function() {
+//     console.log("started");
+// });
 
-// var server = http.createServer(app);
-// server.listen(80);
+var server = http.createServer(app);
+server.listen(80);
